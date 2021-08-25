2effbe2b39c1d4a157d44bb3e119b633af990317
Isaac Freitas <Isaacfrancafreitas@outlook.com>
2021-08-25 07:13:13 -0300

    teste

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,28 +5,50 @@ import {
     Image,
     View
 } from 'react-native';
-import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from "../assets/Isaac.png";
 
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
-export function Header(){
-    return(
+export function Header() {
+    return (
         <View style={styles.container}>
-
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.userName}>Isaac</Text>
+                
+            </View>
+            <Image style={styles.image} source={userImg}/>
         </View>
-    )
+    )   
 }
 
-const styles = StyleSheet.create({
-    container:{
-        width:"100%",
+const styles = StyleSheet.create({ 
+    container: {
+        width: '100%',
         flexDirection: 'row',
-        justifyContent:'space-between',
-        alignItems:'center',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         paddingVertical: 20,
-        marginTop:getStatusBarHeight(),
-        backgroundColor:colors.red
-
+        marginTop: getStatusBarHeight(),
+        padding:20
+    },
+    image:{
+        width:80,
+        height:80,
+        borderRadius:40
+    },
+    greeting:{
+        fontSize:32,
+        color:colors.heading,
+        fontFamily:fonts.text,
+    },
+    userName:{
+        fontSize:32,
+        fontFamily:fonts.heading,
+        color:colors.heading
 
     }
 })
